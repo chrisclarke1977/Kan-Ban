d6e5ec716dfebdcb36eebcbd8d2d39503f11b9a8
chrisclarke1977 <chrisclarke1977@gmail.com>
2014-10-06 09:53:23 +0000

    Vanilla files

---
+++ app/app.js
@@ -0,0 +1,10 @@
+/**
+* Created with Kan-Ban.
+* User: chrisclarke1977
+* Date: 2014-10-06
+* Time: 09:35 AM
+* To change this template use Tools | Templates.
+*/
+define(function() {
+return {};
+});
